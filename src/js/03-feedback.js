2c182cbf7bf5cb69edeefe270240ab630f60a003
CarmenViolet <margaretholmes200@gmail.com>
2022-09-03 13:50:29 +0300

    updated

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,29 +7,31 @@ formEl.addEventListener('submit', submitForm);
 
 const STORAGE_KEY = "feedback-form-state";
 
+// const values = JSON.parse(localStorage.getItem(STORAGE_KEY))
+// console.log(values);
 const storage = {};
+
+function getStorageValues() {
+    const storageData = JSON.parse(localStorage.getItem(STORAGE_KEY))
+ 
+     if(storageData) {
+         formEl.email.value = storageData.email;
+         formEl.message.value = storageData.message;
+         console.log(storageData);
+     }
+ }
+getStorageValues();
+
 function addInformationToStorage(event) {
     storage[event.target.name] = event.target.value;
     localStorage.setItem(STORAGE_KEY, JSON.stringify(storage));
 }
 
 function submitForm(event) {
+    console.log(JSON.parse(localStorage.getItem(STORAGE_KEY))); 
     event.preventDefault();
     formEl.reset();
     localStorage.removeItem(STORAGE_KEY);
 }
 
-function getStorageValues() {
-    // const values = {};
-   const values = JSON.stringify( localStorage.getItem(STORAGE_KEY));
-   console.log(values);
 
-//    const email = document.querySelector('input[type="email"]');
-//    const message = document.querySelector('input[type="submit"]);
-
-//     if(values) {
-//         email.value = values.email;
-//         message.value = values.message;
-//         console.log(values);
-//     }
-}
